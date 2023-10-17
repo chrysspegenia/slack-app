6d11918e6fb20e917f9311a41b2a7f3a9156a4a9
FRANCIS CHRYSS PEGENIA <chrysspegenia@gmail.com>
2023-10-17 21:42:53 +0800

    Modify textbox-section layout and design

--- src/components/Textbox.jsx
+++ src/components/Textbox.jsx
@@ -4,31 +4,42 @@ import './Textbox.css'
 const Textbox = () => {
     return (
         <div className="textbox-section">
-            <div className='buttons-area'>
-                <button className='textbtn bold-btn'><i class="fa-solid fa-bold"></i></button>
-                <button className='textbtn italic-btn'><i class="fa-solid fa-italic"></i></button>
-                <button className='textbtn strikethrough-btn'><i class="fa-solid fa-strikethrough"></i></button>
-                <button className='textbtn link-btn'><i class="fa-solid fa-link"></i></button>
-                <button className='textbtn ol-btn'><i class="fa-solid fa-list-ol"></i></button>
-                <button className='textbtn li-btn'><i class="fa-solid fa-list"></i></button>
-                <button className='textbtn blockquote-btn'><i class="fa-solid fa-bars-staggered"></i></button>
-                <button className='textbtn code-btn'><i class="fa-solid fa-code"></i></button>
-                <button className='textbtn codeblock-btn'><i class="fa-solid fa-laptop-code"></i></button>
-            </div>
-            <input className="input-text" type="text" placeholder='Message'></input>
-            <div className='textbox-lower-section'>
-                <div className='attachments-btn'><i class="fa-solid fa-plus"></i></div>
+            <div className="textbox-container">
                 <div className='buttons-area'>
-                    <button className='textbtn formatting-btn'>Aa</button>
-                    <button className='textbtn emoji-btn'><i class="fa-regular fa-face-smile"></i></button>
-                    <button className='textbtn at-btn'><i class="fa-solid fa-at"></i></button>
-                    <button className='textbtn video-btn'><i class="fa-solid fa-video"></i></button>
-                    <button className='textbtn microphone-btn'><i class="fa-solid fa-microphone"></i></button>
-                    <button className='textbtn shortcut-btn'><i class="fa-regular fa-square"></i></button>
+                    <button className='textbtn textbtn-upper bold-btn'><i className="fa-solid fa-bold"></i></button>
+                    <button className='textbtn textbtn-upper italic-btn'><i className="fa-solid fa-italic"></i></button>
+                    <button className='textbtn textbtn-upper strikethrough-btn'><i className="fa-solid fa-strikethrough"></i></button>
+                    <span className="line-divider"></span>
+                    <button className='textbtn textbtn-upper link-btn'><i className="fa-solid fa-link"></i></button>
+                    <span className="line-divider"></span>
+                    <button className='textbtn textbtn-upper ol-btn'><i className="fa-solid fa-list-ol"></i></button>
+                    <button className='textbtn textbtn-upper li-btn'><i className="fa-solid fa-list"></i></button>
+                    <span className="line-divider"></span>
+                    <button className='textbtn textbtn-upper blockquote-btn'><i className="fa-solid fa-bars-staggered"></i></button>
+                    <span className="line-divider"></span>
+                    <button className='textbtn textbtn-upper code-btn'><i className="fa-solid fa-code"></i></button>
+                    <button className='textbtn textbtn-upper codeblock-btn'><i className="fa-solid fa-laptop-code"></i></button>
                 </div>
-                <div className='confirm-btns'>
-                    <div className='send-btn'><i class="fa-solid fa-paper-plane"></i></div>
-                    <i class="fa-solid fa-chevron-down"></i>
+
+                <textarea className="input-text" type="text" placeholder='Message'></textarea>
+
+                <div className='textbox-lower-section'>
+                    <div className='buttons-area'>
+                        <div className='textbtn attachments-btn'><i className="fa-solid fa-plus"></i></div>
+                        <button className='textbtn formatting-btn'>Aa</button>
+                        <button className='textbtn emoji-btn'><i className="fa-regular fa-face-smile"></i></button>
+                        <button className='textbtn at-btn'><i className="fa-solid fa-at"></i></button>
+                        <span className="line-divider"></span>
+                        <button className='textbtn video-btn'><i className="fa-solid fa-video"></i></button>
+                        <button className='textbtn microphone-btn'><i className="fa-solid fa-microphone"></i></button>
+                        <span className="line-divider"></span>
+                        <button className='textbtn shortcut-btn'><i className="fa-regular fa-square"></i></button>
+                    </div>
+                    <div className='confirm-btns'>
+                        <div className='textbtn send-btn'><i className="fa-solid fa-paper-plane"></i></div>
+                        <span className="line-divider"></span>
+                        <i className="textbtn fa-solid fa-chevron-down"></i>
+                    </div>
                 </div>
             </div>
         </div>
