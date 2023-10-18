985a8bb6c31378b8a52e0942e4907d796e0eb2a2
FRANCIS CHRYSS PEGENIA <chrysspegenia@gmail.com>
2023-10-18 20:52:38 +0800

    Add layout page to contain slack components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import Textbox from './components/Textbox';
-import Layout from './components/Layout';
+import Layout from './pages/Layout';
 
 function App() {
   return (
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Textbox from '../components/Textbox';
+import SearchBox from '../components/SearchBox';
+
+const Layout = () => {
+    return (
+        <div>
+            <SearchBox></SearchBox>
+            <Textbox></Textbox>
+        </div>
+    );
+};
+
+export default Layout;
