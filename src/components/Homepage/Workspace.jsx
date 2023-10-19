89343a7ea4294845db4bb079d9b4834a625d5e3e
FRANCIS CHRYSS PEGENIA <chrysspegenia@gmail.com>
2023-10-19 11:58:19 +0800

    Modify grid layout

--- src/components/Homepage/Workspace.jsx
+++ src/components/Homepage/Workspace.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Workspace.css'
 import Textbox from './Textbox';
+import NavComms from './NavComms';
 
 const Workspace = () => {
     return (
         <div className='workspace-section'>
             <Textbox></Textbox>
+            <NavComms></NavComms>
         </div>
     );
 };
