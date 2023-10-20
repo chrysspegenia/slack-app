cce9d2e18db6b621f690d2444229634f456f1a01
GARS <christian.garcia4297@gmail.com>
2023-10-20 14:25:45 +0800

    updated app.js to conditionally render homepage component  on login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 // import Textbox from './components/Textbox';
+import React from 'react';
+import { useState } from "react";
 import Homepage from './pages/Homepage';
 import Login from './pages/login';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div className="App">
-      <Login></Login>
-      {/* <Textbox /> */}
+      {isLoggedIn ? <Homepage /> : <Login setIsLoggedIn={setIsLoggedIn} />}
     </div>
   );
 }
