132aecae563d877ccf3a6ba144425907c366875e
GARS <christian.garcia4297@gmail.com>
2023-11-13 20:03:41 +0800

    added test for rendering components, modified package.json and installed necessary libraries

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders App Component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const appTitle = screen.getByText(/Slack/i);
+  expect(appTitle).toBeInTheDocument();
 });
