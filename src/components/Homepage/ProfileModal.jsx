5f6406211fd31e88c30acae222df334b52d79de0
FRANCIS CHRYSS PEGENIA <chrysspegenia@gmail.com>
2023-11-14 00:04:38 +0800

    Add layout design to profileModal component

--- src/components/Homepage/ProfileModal.jsx
+++ src/components/Homepage/ProfileModal.jsx
@@ -12,10 +12,17 @@ const ProfileModal = (props) => {
 
     return (
         <div className='profile-modal'>
-            <i className="user-icon fa-regular fa-user"></i>
-            <p className='profile-email'>{user.uid}</p>
-            <p className='profile-email'>{user.id}</p>
-            <button onClick={logout}>Logout</button>
+            <div className='profile-info-section'>
+                <i className="user-icon fa-regular fa-user"></i>
+                <p className='profile-info profile-email'>{user.uid}</p>
+                <p className='profile-info profile-id'>{user.id}</p>
+            </div>
+
+            <p className='profile-btns'>Set yourself as active</p>
+            <p className='profile-btns'>Pause notification</p>
+            <p className='profile-btns'>Profile</p>
+            <p className='profile-btns'>Preferences</p>
+            <button className="logout-btn" onClick={logout}>Sign out of Slack</button>
         </div>
     );
 };
