9455678ec03e33881f01589fddb107fe0e3754a0
GARS <christian.garcia4297@gmail.com>
2023-11-13 20:27:06 +0800

    modified tests

--- src/components/Homepage/MessageArea.test.jsx
+++ src/components/Homepage/MessageArea.test.jsx
@@ -2,6 +2,6 @@ import { act } from 'react-dom/test-utils';
 import { render, screen } from '@testing-library/react';
 import MessageArea from "./MessageArea";
 
-test ("render NavComms", () => {
+test ("render MessageArea", () => {
     render(<MessageArea />);
 })
