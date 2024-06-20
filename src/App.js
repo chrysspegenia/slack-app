293621185388015dba0a51053e347969b561d51d
Francis Chryss Pegenia <chrysspegenia@gmail.com>
2024-06-20 23:06:09 +0800

    Modify login import path

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import './App.css';
+import "./App.css";
 // import Textbox from './components/Textbox';
-import React from 'react';
+import React from "react";
 import { useState } from "react";
-import Homepage from './pages/Homepage';
-import Login from './pages/Login';
+import Homepage from "./pages/Homepage";
+import Login from "./pages/login";
 
 function App() {
-  const [user, setUser] = useState(
-    () => JSON.parse(localStorage.getItem("user") || null)
+  const [user, setUser] = useState(() =>
+    JSON.parse(localStorage.getItem("user") || null)
   );
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="App">
       {isLoggedIn ? (
-        <Homepage 
-          user={user} 
-          setIsLoggedIn={setIsLoggedIn} 
-        />
+        <Homepage user={user} setIsLoggedIn={setIsLoggedIn} />
       ) : (
-        <Login 
-          user={user} 
-          setUser={setUser} 
-          setIsLoggedIn={setIsLoggedIn} />
+        <Login user={user} setUser={setUser} setIsLoggedIn={setIsLoggedIn} />
       )}
     </div>
   );
